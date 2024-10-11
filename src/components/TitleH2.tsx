acc1830486e069ed1c8b7c048f6efe9b71f813ab
unknown <jordan.conot@gmail.com>
2024-10-11 13:32:31 +0200

    feat: added component title h2

---
+++ src/components/TitleH2.tsx
@@ -0,0 +1,8 @@
+
+export default function TitleH2() {
+  return (
+    <h2>
+      Create Employee
+    </h2>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import TitleH1 from "../components/TitleH1";
+import TitleH2 from "../components/TitleH2";
 
 export default function Home() {
   return (
     <div>
       <TitleH1 text="HRnet" />
+      <TitleH2 />
     </div>
   )
 }
