a225ceee79396dae451d96b7318a636b442b9921
unknown <jordan.conot@gmail.com>
2024-10-18 13:03:07 +0200

    feat: added searchbar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => onSearch(e.target.value)}
+        className="form-control"
+      />
+    </div>
+  )
+}
