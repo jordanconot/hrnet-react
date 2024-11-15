9ae84862be8a2eae6cf4a73b1ab32d9d1dba35d2
unknown <jordan.conot@gmail.com>
2024-11-15 19:56:57 +0100

    feat: dependency optimisation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('react-datepicker')) {
+            return 'datepicker';
+          }
+          if (id.includes('react-select')) {
+            return 'select'
+          }
+        }
+      }
+    }
+  }
 })
