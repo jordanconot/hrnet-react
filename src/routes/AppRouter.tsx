bf0d8e0e8b758d6d90c4c42e01b6327adbcce5cc
unknown <jordan.conot@gmail.com>
2024-11-21 20:48:24 +0100

    fix: 404 page when refresh

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import Employees from '../pages/Employees';
 import Home from '../pages/Home';
 
 export default function AppRouter() {
   return (
-    <Router basename='/hrnet-react'>
+    <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='employees-list' element={<Employees />} />
